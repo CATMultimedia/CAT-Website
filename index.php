79ffea0bcd47cbabf7228731e8475bc3bddf51a9
Alonso F <alonsofedz@gmail.com>
2016-07-02 12:16:22 -0500

    Index and Name Logo
    - Create the index that will pull information from the elements
    - Upload the Logo Name SVG

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Colegio Americano de Torreón</title>
+        <link rel="stylesheet" href"/css/styles.css" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Ubuntu:400,500,700,300" rel="stylesheet" type="text/css">
+    </head>
+
+	<?php include "_elements/head-foot.html" ;?>
+	<?php include "_elements/home.html" ;?>
+	<?php include "_elements/head-foot.html" ;?>
