ddf4f67b47b9624700a4f5f46c6070326cb7c53d
Alonso Fedz <alonsofedz@gmail.com>
2016-07-06 13:31:57 -0500

    Putting it all together

--- index.php
+++ index.php
@@ -6,8 +6,19 @@
         <title>Colegio Americano de Torreón</title>
         <link rel="stylesheet" href="/css/base.css" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Ubuntu:400,500,700,300" rel="stylesheet" type="text/css">
+		<script async type="text/javascript">
+		  WebFontConfig={google:{ families:['Ubuntu:300,400,400italic,500,500italic,700,700italic:latin' ]}};(function() {var wf = document.createElement('script');wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';wf.type = 'text/javascript';wf.async = 'true';var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(wf, s);})();
+		  </script>
+
     </head>
 
-	<?php include "_elements/head-foot.html" ;?>
+    <body>
+
+	<?php include "_elements/header.html" ;?>
 	<?php include "_elements/home.html" ;?>
-	<?php include "_elements/head-foot.html" ;?>
+	<?php include "_elements/footer.html" ;?>
+
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js"></script>
+	<script src="../js/min/script-min.js" type="text/javascript"></script>
+    </body>
+</html>
