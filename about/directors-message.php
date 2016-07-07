6030377ddf75daf833b108f09567bc96b0068d26
Alonso Fedz <alonsofedz@gmail.com>
2016-07-06 20:00:57 -0500

    Create directors-message to simulate interior page

---
+++ about/directors-message.php
@@ -0,0 +1,48 @@
+<?php
+	$pagetitle = "Director's Message";
+	include "../_elements/header.php"; ?>
+
+<main class="inside">
+    <section class="inside-content">
+	        <h1 class="page-title">Main Page Title</h1>
+		<div class="content">
+			<p class="page-hero">This is a page hero. Aenean varius augue convallis vestibulum natoque risus magna quisque fermentum.</p>
+			<p>This is regular paragraph text. Augue pulvinar faucibus duis vulputate penatibus nullam suscipit iaculis commodo urna. Erat curae; placerat nunc vehicula. Himenaeos adipiscing ultrices elit suscipit, mattis praesent lobortis. Mi ligula molestie vel et lobortis elementum lectus tellus tempus enim. Vulputate quisque ante ridiculus etiam ultrices enim, litora dignissim elit volutpat quis class? Quam dictum rutrum netus.</p>
+
+			<blockquote>
+			  This is an example quotation that uses the blockquote tag. Augue pulvinar faucibus duis vulputate penatibus nullam suscipit iaculis commodo urna.
+			</blockquote>
+
+			<p>This is regular paragraph text. Augue pulvinar faucibus duis vulputate penatibus nullam suscipit iaculis commodo urna. Erat curae; placerat nunc vehicula. Himenaeos adipiscing ultrices elit suscipit, mattis praesent lobortis. Mi ligula molestie vel et lobortis elementum lectus tellus tempus enim. Vulputate quisque ante ridiculus etiam ultrices enim, litora dignissim elit volutpat quis class? Quam dictum rutrum netus.</p>
+			<p>This is regular paragraph text. Augue pulvinar faucibus duis vulputate penatibus nullam suscipit iaculis commodo urna. Erat curae; placerat nunc vehicula. Himenaeos adipiscing ultrices elit suscipit, mattis praesent lobortis. Mi ligula molestie vel et lobortis elementum lectus tellus tempus enim. Vulputate quisque ante ridiculus etiam ultrices enim, litora dignissim elit volutpat quis class? Quam dictum rutrum netus.</p>
+
+			<blockquote>
+			  This is an example quotation that uses the blockquote tag. Augue pulvinar faucibus duis vulputate penatibus nullam suscipit iaculis commodo urna.
+			</blockquote>
+
+			<p>This is regular paragraph text. Augue pulvinar faucibus duis vulputate penatibus nullam suscipit iaculis commodo urna. Erat curae; placerat nunc vehicula. Himenaeos adipiscing ultrices elit suscipit, mattis praesent lobortis. Mi ligula molestie vel et lobortis elementum lectus tellus tempus enim. Vulputate quisque ante ridiculus etiam ultrices enim, litora dignissim elit volutpat quis class? Quam dictum rutrum netus.</p>
+			<p>This is regular paragraph text. Augue pulvinar faucibus duis vulputate penatibus nullam suscipit iaculis commodo urna. Erat curae; placerat nunc vehicula. Himenaeos adipiscing ultrices elit suscipit, mattis praesent lobortis. Mi ligula molestie vel et lobortis elementum lectus tellus tempus enim. Vulputate quisque ante ridiculus etiam ultrices enim, litora dignissim elit volutpat quis class? Quam dictum rutrum netus.</p>
+		</div>
+    </section>
+
+    <section class="inside-supporting">
+		<aside class="aside">
+			<h1 class="aside-title">Titulo de un aside</h1>
+			<section class="aside-content">
+				Lacus curae; neque sit pellentesque scelerisque laoreet accumsan suspendisse vehicula bibendum per. Maecenas class enim vitae. Dictumst; ornare posuere eleifend luctus accumsan consequat quam dui sit ullamcorper. Ut per cubilia, parturient vehicula bibendum elit quisque porta vel. Augue sem venenatis praesent dictum leo condimentum eu eleifend cursus. Volutpat iaculis diam est iaculis magna odio facilisi, eget facilisi fermentum mauris porttitor! Maecenas nullam euismod metus sed purus? Elementum enim, fringilla nunc.
+			</section>
+		</aside>
+
+		<aside class="aside">
+			<h1 class="aside-title">Titulo de un aside</h1>
+			<section class="aside-content">
+				Lacus curae; neque sit pellentesque scelerisque laoreet accumsan suspendisse vehicula bibendum per. Maecenas class enim vitae. Dictumst; ornare posuere eleifend luctus accumsan consequat quam dui sit ullamcorper. Ut per cubilia, parturient vehicula bibendum elit quisque porta vel. Augue sem venenatis praesent dictum leo condimentum eu eleifend cursus. Volutpat iaculis diam est iaculis magna odio facilisi, eget facilisi fermentum mauris porttitor! Maecenas nullam euismod metus sed purus? Elementum enim, fringilla nunc.
+			</section>
+		</aside>
+
+    </section>
+</main>
+<?php
+	include "../_elements/footer.html";
+
+?>
