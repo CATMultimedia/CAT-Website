47311a9eb26e717ea12ac28665e734eb3bc1d985
Alonso Fedz <alonsofedz@gmail.com>
2016-07-06 23:22:12 -0500

    Mobile Menu fix?

--- js/head-foot.js
+++ js/head-foot.js
@@ -11,9 +11,16 @@ var menu = document.querySelector("nav.menu");
 
 function showMenu(){
     menu.className = "menu show";
+    document.body.classList.toggle("no-scroll");
+	// document.body.addEventListener("transitionend", afterTransition, true);//firefox
+	// document.body.addEventListener("webkitTransitionEnd", afterTransition, true);//chrome
+	function afterTransition(){
+	  document.body.classList.toggle("disable");
+	}
 }
 
 function hideMenu(){
+    document.body.classList.toggle("no-scroll");
     menu.className = "menu";
 }
 
@@ -23,11 +30,11 @@ quicklinkBtn.addEventListener("click", showQL);
 
 function showQL() {
     var qll = document.querySelector("ul.quicklinks");
-    
+
     if (qll.className === "quicklinks"){
         qll.className = "quicklinks expand"; document.querySelector("#quicklink").style.color = "rgba(255, 255, 255, 0.7)";
     }
-    
+
     else {
         qll.className = "quicklinks"; document.querySelector("#quicklink").style.color = "white";
     }
