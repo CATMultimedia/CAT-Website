cffde26004979e256c1b7f13f54ce4ec9f419216
ValenteAlvarez <valentealvarez1@gmail.com>
2016-08-08 15:51:16 -0500

    deleted necessary for loop
    I had forgotten to delete a for loop that was unnecessary. It’s gone
    now.

--- js/accordion.js
+++ js/accordion.js
@@ -1,8 +1,6 @@
 //Accordion menu
 var mainLinks = document.querySelectorAll('li.mainLink');
-for (var x=0; x < mainLinks.length; x++){
-    mainLinks[x].addEventListener('click', accordion);
-}
+document.querySelector('ul.menu').addEventListener('click', accordion);
 function accordion(e) {
     var tElem = e.target;  
     if (tElem.className === "mainLink"){
