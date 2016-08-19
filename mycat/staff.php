49256792eb192d6da35deecced598432aa24caf5
Alonso Fedz <alonsofedz@gmail.com>
2016-08-19 14:46:01 -0500

    Fix MyCAT Staff

--- mycat/staff.php
+++ mycat/staff.php
@@ -35,22 +35,19 @@ if( isset($_SESSION["gp_access_token"]) ){ // If logged in
     <section class="inside-content">
 	        <h1 class="page-title">Welcome to MyCAT</h1>
 		<div class="content">
-			<p class="page-hero">This is a page hero. Aenean varius augue convallis vestibulum natoque risus magna quisque fermentum.</p>
-			<p>This is regular paragraph text. Augue pulvinar faucibus duis vulputate penatibus nullam suscipit iaculis commodo urna. Erat curae; placerat nunc vehicula. Himenaeos adipiscing ultrices elit suscipit, mattis praesent lobortis. Mi ligula molestie vel et lobortis elementum lectus tellus tempus enim. Vulputate quisque ante ridiculus etiam ultrices enim, litora dignissim elit volutpat quis class? Quam dictum rutrum netus.</p>
+			<p class="page-hero">Welcome to the CAT Teachers' Portal with everything you could possibly need.</p>
+			<h2>Calendars</h2>
+			<p>
+				<span class="cal-swatch" style="background:#2952A3">&nbsp;&nbsp;&nbsp;&nbsp;</span> Facilities &nbsp;&nbsp;&nbsp;
+				<span class="cal-swatch" style="background:#711616">&nbsp;&nbsp;&nbsp;&nbsp;</span> Laptop &nbsp;&nbsp;&nbsp;
+				<span class="cal-swatch" style="background:#B1440E">&nbsp;&nbsp;&nbsp;&nbsp;</span> iPads &nbsp;&nbsp;&nbsp;
+				<span class="cal-swatch" style="background:#0F4B38">&nbsp;&nbsp;&nbsp;&nbsp;</span> Projector &nbsp;&nbsp;&nbsp;
+				<span class="cal-swatch" style="background:#528800">&nbsp;&nbsp;&nbsp;&nbsp;</span> Room 54 &nbsp;&nbsp;&nbsp;
+			</p>
+			<iframe src="https://calendar.google.com/calendar/embed?title=CAT%20Events&amp;showTitle=0&amp;showPrint=0&amp;showTabs=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=cat.mx_53ffcqgmisee7235qgof100ue8%40group.calendar.google.com&amp;color=%232952A3&amp;src=cat.mx_0k30e4vnurau53tunkoofgfg0o%40group.calendar.google.com&amp;color=%23711616&amp;src=cat.mx_07re1ol9pm61eavu18qsl0n0hk%40group.calendar.google.com&amp;color=%23B1440E&amp;src=cat.mx_uaipuhtbova20co179nnc54lj4%40group.calendar.google.com&amp;color=%230F4B38&amp;src=cat.mx_kia5241n372cq1vl0jn0fjnnf0%40group.calendar.google.com&amp;color=%23528800&amp;ctz=America%2FMexico_City" style="border-width:0" width="90%" height="500" frameborder="0" scrolling="no" class="cal"></iframe>
+			<div class="portal-container">
 
-			<blockquote>
-			  This is an example quotation that uses the blockquote tag. Augue pulvinar faucibus duis vulputate penatibus nullam suscipit iaculis commodo urna.
-			</blockquote>
-
-			<p>This is regular paragraph text. Augue pulvinar faucibus duis vulputate penatibus nullam suscipit iaculis commodo urna. Erat curae; placerat nunc vehicula. Himenaeos adipiscing ultrices elit suscipit, mattis praesent lobortis. Mi ligula molestie vel et lobortis elementum lectus tellus tempus enim. Vulputate quisque ante ridiculus etiam ultrices enim, litora dignissim elit volutpat quis class? Quam dictum rutrum netus.</p>
-			<p>This is regular paragraph text. Augue pulvinar faucibus duis vulputate penatibus nullam suscipit iaculis commodo urna. Erat curae; placerat nunc vehicula. Himenaeos adipiscing ultrices elit suscipit, mattis praesent lobortis. Mi ligula molestie vel et lobortis elementum lectus tellus tempus enim. Vulputate quisque ante ridiculus etiam ultrices enim, litora dignissim elit volutpat quis class? Quam dictum rutrum netus.</p>
-
-			<blockquote>
-			  This is an example quotation that uses the blockquote tag. Augue pulvinar faucibus duis vulputate penatibus nullam suscipit iaculis commodo urna.
-			</blockquote>
-
-			<p>This is regular paragraph text. Augue pulvinar faucibus duis vulputate penatibus nullam suscipit iaculis commodo urna. Erat curae; placerat nunc vehicula. Himenaeos adipiscing ultrices elit suscipit, mattis praesent lobortis. Mi ligula molestie vel et lobortis elementum lectus tellus tempus enim. Vulputate quisque ante ridiculus etiam ultrices enim, litora dignissim elit volutpat quis class? Quam dictum rutrum netus.</p>
-			<p>This is regular paragraph text. Augue pulvinar faucibus duis vulputate penatibus nullam suscipit iaculis commodo urna. Erat curae; placerat nunc vehicula. Himenaeos adipiscing ultrices elit suscipit, mattis praesent lobortis. Mi ligula molestie vel et lobortis elementum lectus tellus tempus enim. Vulputate quisque ante ridiculus etiam ultrices enim, litora dignissim elit volutpat quis class? Quam dictum rutrum netus.</p>
+			</div>
 		</div>
     </section>
 
@@ -58,15 +55,27 @@ if( isset($_SESSION["gp_access_token"]) ){ // If logged in
 		<aside class="aside">
 			<h1 class="aside-title">Hello <?php echo $first_name.' '.$last_name; ?>!<img src="<?php echo $profile_image_link; ?>" class="aside-profile-img"></h1>
 			<section class="aside-content">
+				 <p class="align-right portal-logout"><a href="/mycat/logout.php" class="btn btn-small">Logout</a></p>
 				 <p>Your Email: <?php echo $email; ?></p>
-				 <p><a href="/mycat/logout.php" class="btn">Logout</a></p>
+				 <br />
+				 <p>
+					 <span class="portal-icon"><a href="http://plusportals.com/cat" data-title="Teacher's Plus"><img src="/img/portal/teachersplus.png"></a></span>
+					 <span class="portal-icon"><a href="http://mail.google.com" data-title="Gmail"><img src="/img/portal/email.png"></a></span>
+					 <span class="portal-icon"><a href="http://drive.google.com" data-title="Gmail"><img src="/img/portal/drive.png"></a></span>
+					 <span class="portal-icon"><a href="http://docs.google.com" data-title="Google Docs"><img src="/img/portal/docs.png"></a></span>
+					 <span class="portal-icon"><a href="http://slides.google.com" data-title="Google Slides"><img src="/img/portal/slides.png"></a></span>
+					 <span class="portal-icon"><a href="http://sheets.google.com" data-title="Google Sheets"><img src="/img/portal/sheets.png"></a></span>
+				 </p>
 			</section>
 		</aside>
 
 		<aside class="aside">
-			<h1 class="aside-title">Titulo de un aside</h1>
+			<h1 class="aside-title">Quick Documents</h1>
 			<section class="aside-content">
-				Lacus curae; neque sit pellentesque scelerisque laoreet accumsan suspendisse vehicula bibendum per. Maecenas class enim vitae. Dictumst; ornare posuere eleifend luctus accumsan consequat quam dui sit ullamcorper. Ut per cubilia, parturient vehicula bibendum elit quisque porta vel. Augue sem venenatis praesent dictum leo condimentum eu eleifend cursus. Volutpat iaculis diam est iaculis magna odio facilisi, eget facilisi fermentum mauris porttitor! Maecenas nullam euismod metus sed purus? Elementum enim, fringilla nunc.
+				<ul>
+					<li><a href="http://www.cat.mx/handbooks/TEACHER_HANDBOOK_16-17.pdf">Teacher Handbook (2016-2017)</a></li>
+					<li><a href="http://www.cat.mx/Community-Teachers-Professional%20Development.pdf">Professional Development Policies</a></li>
+					<li><a href="http://www.cat.mx/Formato_para_verificacion_de_creditos.pdf">PD Credit Verification Format</a></li>
 			</section>
 		</aside>
 
