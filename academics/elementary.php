a0f7e532ec08d7fcf60bc48ce088405b6ea78e3d
Alonso Fedz <alonsofedz@gmail.com>
2016-08-19 14:45:45 -0500

    Remove counselling from Elementary and move to Counselling

--- academics/elementary.php
+++ academics/elementary.php
@@ -104,15 +104,6 @@
 				</ul>
 			</section>
 		</aside>
-
-		<aside class="aside">
-			<h1 class="aside-title">Counseling</h1>
-			<section class="aside-content">
-				<p>The Elementary Counseling department is considered a service with a specific program which has been designed and organized principally to aid in the student’s adaptation to the particular school environment encountered here.</p>
-				<p>This program provides the necessary instruments for each student’s emotional well being within his/her educational experience. The school system should guarantee that every student has the opportunity to be understood and treated as an individual and to aid students to resolve individual concerns or problem areas within the framework of the process of maturation. The prevention of problems is a fundamental goal of the counseling program. The guidance or counseling program has to try to help each student take effective advantage of his/her school experiences and to use the knowledge he/she acquires in the best possible way.</p>
-			</section>
-		</aside>
-
     </section>
 
 </main>
